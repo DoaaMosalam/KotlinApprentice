1814ee3b87b358118d9c950b4879f3690ee8cbad
Doaa Mosalam <DoaaMosalam9@gmail.com>
2025-06-13 18:07:47 +0300

    the Kotlin Apprentice book step-by-step and solving every exercise to build a strong foundation in the Kotlin programming language.

--- src/chapterOne/average.kt
+++ src/chapterOne/average.kt
@@ -1,7 +1,6 @@
 package chapterOne
 
 fun main() {
-   
     val a:Double=23.8
     val b:Double=29.8
     val average = a+b/2
--- src/chapterOne/chessboard.kt
+++ src/chapterOne/chessboard.kt
@@ -5,7 +5,7 @@ numbered top to bottom, 0 to 7. Columns are numbered left to right, 0 to 7. Decl
 a constant position and assign it a value between 0 and 63. Calculate the
 corresponding row and column numbers and store the results in constants named
 row and column.*/
-fun main(args: Array<String>) {
+fun main() {
      val position: Int = 32
      val row = position / 8
      val column = position % 8
